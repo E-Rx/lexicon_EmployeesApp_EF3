2b73c8bdb1eece291505aa76dffb02a5d5293dba
Tobias Johansson <tobias@noq.nu>
2025-06-04 15:59:51 +0200

    Add company management features and views
    Implemented CompaniesController with dependency injection for ICompanyService.
    Expanded service registration in Program.cs to include company-related services.
    Created CompanyIndex view and view model for displaying a list of companies.
    Updated _ViewImports.cshtml to include necessary namespaces for new views.

---
+++ EmployeesApp.Web/Controllers/CompaniesController.cs
@@ -0,0 +1,25 @@
+﻿using EmployeesApp.Application.Employees.Interfaces;
+using EmployeesApp.Web.Views.Companies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeesApp.Web.Controllers;
+public class CompaniesController(ICompanyService service) : Controller
+{
+    [HttpGet("companies")]
+    public async Task<IActionResult> CompanyIndexAsync()
+    {
+        var model = await service.GetAllAsync();
+
+        var viewModel = new CompanyIndexVM()
+        {
+            CompaniesVMs = [.. model
+            .Select(e => new CompanyIndexVM.CompanyVM()
+            {
+                Id = e.Id,
+                CompanyName = e.CompanyName != null ? e.CompanyName : "N/A",
+            })]
+        };
+
+        return View(viewModel);
+    }
+}
---
+++ EmployeesApp.Web/Views/Companies/CompanyIndexVM.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeesApp.Web.Views.Companies;
+
+public class CompanyIndexVM
+{
+    public CompanyVM[] CompaniesVMs { get; set; } = null!;
+
+    public class CompanyVM
+    {
+        public required int Id { get; set; }
+        public string? CompanyName { get; set; }
+    }
+}
